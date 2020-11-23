307a6fb3a1c629c2cdd50ddf84de1b26d20556d3
lagofelipe <lagofelipe@gmail.com>
2020-11-23 11:53:53 -0600

    done

--- src/index.js
+++ src/index.js
@@ -5,41 +5,92 @@ const dogsUrl =  "http://localhost:3000/dogs";
 //1
 document.addEventListener('DOMContentLoaded', function(e) {
    fetchDogsUrl();
+submitEditedForm();
 });
 
 
 //fetch dogs url 
 //2
-function fetchDogsUrl(){
-    let table = document.getElementById("table-body")
-    fetch(dogsUrl)
-  .then(function(response){
-      return response.json();
- })
-  .then(function(dogData){
-  
-    for(const dog of dogData){
-         #console.log(dog)
-     
-    let tableRow = document.createElement('tr');
-    table.append(tableRow);
-    tableRow.innerHTML = `<td> ${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button>Edit</button></td>`
-    tableRow.append(editButton)
-    
+function fetchAllDogs(){
+ fetch(dogsUrl)
+.then(res => res.json())
+.then(data => renderAllDogs(data))
+};
 
 
+function renderAllDogs (data){
+const tableBody = document.getElementByID('table-body');
 
-  }
-  })
-}
+data.forEach(dog => {
 
-//2
-// function tableBuilder(name, breed, sex, id){
-// const newTable = document.getElementById("table-body")
+    const tr = document.createElement('tr');
+    tr.id = dog.id;
 
+    const dogName = document.createElement('td');
+    const dogBreed = document.createElement('td');
+    const dogSex = document.createElement('td');
+    const editBtncontainer = document.createElement('td');
+    const editBtn = document.createElement('button');
 
+    dogName.innerText = dog.name;
+    dogBreed.innerText = dog.breed;
+    dogSex.innerText = dog.sex;
+    editBtn.innerText = 'Edit Dog';
 
-// }
+    editBtn.classList.add('edit-btn')
+    dogName.id = 'name';
+    dogBreed.id = 'breed';
+    dogSex.id = 'sex';
+
+    editBtncontainer.appendChild(editBtn);
+    tr.appendChild(dogName);
+    tr.appendChild(dogBreed);
+    tr.appendChild(dogSex);
+    tr.appendChild(editBtncontainer);
+
+    tableBody.appendChild('tr');
+    editBtnListener(editBtn);
+})
+};
+
+function editBtnListener(button){
+const form = document.getElementById('dog-form')
+
+button.addEventListener('click', (event) => {
+    const tableRow = event.target.parentNode.parentNode.children;
+    const name = tableRow.name;
+    const breed = tableRow.breed;
+    const sex = tableRow.sex;
+
+    form.name.value = name.innerText;
+    form.breed.value = breed.innerText;
+    form.sex.value = sex.innerText;
+    form.onsubmit.id = `${event.target.parentNode.parentNode.id}`;
+
+    submitEditedForm(tableRow);
+})
+};
+
+function submitEditedForm(tableRow){
+const form = document.getElementById('dog-form');
+
+form.addEventListener('submit',(event) =>{
+fetch(dogUrl + `/${form.submit.id}`, {
+    method:  'PATCH',
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+        name: form.name.value,
+        breed: form.breed.value,
+        sex: form.sex.value
+    })
+})
+.then(res => res.json())
+.then(data => renderAllDogs)
+
+});
+};
 
 
 
